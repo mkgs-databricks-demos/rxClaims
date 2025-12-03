83c37bf5efa5e5e78e13ce88cc4b0e9eff1449e3
matthew-gigl-db <matthew.giglia@databricks.com>
2025-12-03 16:36:07 +0000

    add file types as a directory and style

--- ncpdp/src/ncpdp_etl/transformations/autoload.py
+++ ncpdp/src/ncpdp_etl/transformations/autoload.py
@@ -3,17 +3,21 @@ from pyspark.sql.functions import col
 
 from utilities.utils import Bronze
 
-BronzePipeline = Bronze(
-    spark = spark
-    ,catalog = spark.conf.get("catalog_use")
-    ,schema = spark.conf.get("schema_use")
-    ,volume = spark.conf.get("volume_use")
-    ,volume_sub_path = spark.conf.get("volume_sub_path_use")
-)
+file_types = spark.conf.get("file_types").split(',')
+file_types = [file_type.strip() for file_type in file_types]
 
-BronzePipeline.stream_ingest()
-BronzePipeline.variant_transform()
-BronzePipeline.extract_requests()
-BronzePipeline.extract_responses()
-BronzePipeline.extract_supplemental()
+for file_type in file_types:
+    BronzePipeline = Bronze(
+        spark = spark
+        ,catalog = spark.conf.get("catalog_use")
+        ,schema = spark.conf.get("schema_use")
+        ,volume = spark.conf.get("volume_use")
+        ,volume_sub_path = f"{spark.conf.get('volume_sub_path_use'}/{file_type})"
+    )
+
+    BronzePipeline.stream_ingest()
+    BronzePipeline.variant_transform()
+    BronzePipeline.extract_requests()
+    BronzePipeline.extract_responses()
+    BronzePipeline.extract_supplemental()
 
