52daaf6c5ac9df348fed2f7ab8a572f1a3bbd434
matthew-gigl-db <matthew.giglia@databricks.com>
2025-12-04 09:26:19 +0000

    fix indentatations

--- ncpdp/src/ncpdp_specifications/utilities/utils.py
+++ ncpdp/src/ncpdp_specifications/utilities/utils.py
@@ -27,6 +27,8 @@ from pyspark.sql.functions import (
     ,from_xml
     ,array_contains
     ,ai_parse_document
+    ,lower
+    ,regexp_extract
 )
 from typing import Any
 
@@ -55,47 +57,48 @@ class DocumentParsing:
         return f"DocumentParsing(catalog='{self.catalog}', schema='{self.schema}', volume='{self.volume}',volume_sub_path='{self.volume_sub_path}')"
       
     def stream_ingest(self):
-      schema_definition = f"""
-        spec_file_source_id STRING NOT NULL PRIMARY KEY COMMENT 'Unique identifier for the ingested file.',
-        file_metadata STRUCT < file_path: STRING, 
-        file_name: STRING,
-        file_size: BIGINT,
-        file_block_start: BIGINT,
-        file_block_length: BIGINT,
-        file_modification_time: TIMESTAMP > NOT NULL COMMENT 'Metadata about the file ingested.'
-        ,ingest_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date timestamp the file was ingested.'
-        ,path STRING COMMENT 'The path to the file starting with dbfs:/'
-        ,modificationTime TIMESTAMP COMMENT 'The date timestamp the file was modified.'
-        ,length LONG COMMENT 'Length of the file in bytes.'
-        ,content BINARY COMMENT 'Binary representation of the file.'
-      """
+        schema_definition = f"""
+            spec_file_source_id STRING NOT NULL PRIMARY KEY COMMENT 'Unique identifier for the ingested file.',
+            file_metadata STRUCT < file_path: STRING, 
+            file_name: STRING,
+            file_size: BIGINT,
+            file_block_start: BIGINT,
+            file_block_length: BIGINT,
+            file_modification_time: TIMESTAMP > NOT NULL COMMENT 'Metadata about the file ingested.'
+            ,ingest_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date timestamp the file was ingested.'
+            ,path STRING COMMENT 'The path to the file starting with dbfs:/'
+            ,modificationTime TIMESTAMP COMMENT 'The date timestamp the file was modified.'
+            ,length LONG COMMENT 'Length of the file in bytes.'
+            ,content BINARY COMMENT 'Binary representation of the file.'
+        """
 
-      if self.volume_sub_path == None:
-        volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}"
-      else:
-        volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}/{self.volume_sub_path}"
+        if self.volume_sub_path == None:
+            volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}"
+        else:
+            volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}/{self.volume_sub_path}"
 
-      @dp.table(
-        name=f"{self.catalog}.{self.schema}.specification_documents",
-        comment=f"Streaming ingestion of NCPDP Specification Documents.",
-        # spark_conf={"<key>" : "<value>", "<key>" : "<value>"},
-        table_properties=self.table_properties,
-        # path="<storage-location-path>",
-        # partition_cols=["<partition-column>", "<partition-column>"],
-        cluster_by = ["path"],
-        cluster_by_auto=True,
-        schema=schema_definition,
-        # row_filter = "row-filter-clause",
-        temporary=False
-      )
-      # @dp.expect(...)
-      def stream_ingest_function():
-          return (self.spark.readStream
-            .format("cloudFiles")
-            .option("cloudFiles.format", "binaryFile")
-            .load(volume_path)
-            .selectExpr("sha2(concat(_metadata.*), 256) as spec_file_source_id", "_metadata as file_metadata", "*")
-          )
+        @dp.table(
+            name=f"{self.catalog}.{self.schema}.specification_documents",
+            comment=f"Streaming ingestion of NCPDP Specification Documents.",
+            # spark_conf={"<key>" : "<value>", "<key>" : "<value>"},
+            table_properties=self.table_properties,
+            # path="<storage-location-path>",
+            # partition_cols=["<partition-column>", "<partition-column>"],
+            cluster_by = ["path"],
+            cluster_by_auto=True,
+            schema=schema_definition,
+            # row_filter = "row-filter-clause",
+            temporary=False
+        )
+        # @dp.expect(...)
+        def stream_ingest_function():
+            return (
+                self.spark.readStream
+                .format("cloudFiles")
+                .option("cloudFiles.format", "binaryFile")
+                .load(volume_path)
+                .selectExpr("sha2(concat(_metadata.*), 256) as spec_file_source_id", "_metadata as file_metadata", "*")
+            )
 
     def parse_documents(self): 
         @dp.temporary_view(
